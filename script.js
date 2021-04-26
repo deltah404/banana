d038364bacba09f736fd60eddb233c9e7189e6f7
deltah404 <oloskossi@gmail.com>
2021-04-26 16:00:38 +0100

    bana

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ let counter = 0;
 
 function addCounter() {
     counter++;
-    document.getElementById('ticker').innerHTML = counter;
+    document.getElementById('ticker').innerHTML = "you have " + counter + " bananas";
 }
